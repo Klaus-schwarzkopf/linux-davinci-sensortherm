e4a24de66a43e6159437684bd1d4134769b9a1c0
Natarajan Sugumar <sugumar@ti.com>
2011-03-14 15:48:10 +0530

    PWM: Add cpu frequency transition support for PWM devices
    Implement a generic frequency transition callback for PWM
    devices. Generic PWM framework notifies all the PWM devices
    currently being used about the frequency transition.
    
    Signed-off-by: Natarajan Sugumar <sugumar@ti.com>

--- drivers/pwm/pwm.c
+++ drivers/pwm/pwm.c
@@ -30,6 +30,7 @@
 #include <linux/list.h>
 #include <linux/sched.h>
 #include <linux/platform_device.h>
+#include <linux/cpufreq.h>
 #include <linux/pwm/pwm.h>
 
 static const char *REQUEST_SYSFS = "sysfs";
@@ -636,6 +637,27 @@ static struct class pwm_class = {
 	.class_attrs = pwm_class_attrs,
 };
 
+static int pwm_freq_transition_notifier_cb(struct notifier_block *nb,
+		unsigned long val, void *data)
+{
+	struct pwm_device *p;
+
+	p = container_of(nb, struct pwm_device, freq_transition);
+
+	if (val == CPUFREQ_POSTCHANGE && pwm_is_requested(p))
+		p->ops->freq_transition_notifier_cb(p);
+
+	return 0;
+}
+
+static inline int pwm_cpufreq_notifier_register(struct pwm_device *p)
+{
+	p->freq_transition.notifier_call = pwm_freq_transition_notifier_cb;
+
+	return cpufreq_register_notifier(&p->freq_transition,
+		       CPUFREQ_TRANSITION_NOTIFIER);
+}
+
 int pwm_register_byname(struct pwm_device *p, struct device *parent,
 			const char *name)
 {
@@ -665,6 +687,11 @@ int pwm_register_byname(struct pwm_device *p, struct device *parent,
 
 	dev_set_drvdata(p->dev, p);
 	p->flags = BIT(FLAG_REGISTERED);
+
+	ret = pwm_cpufreq_notifier_register(p);
+
+	if (ret < 0)
+		printk(KERN_ERR "Failed to add cpufreq notifier\n");
 
 	goto done;
 
@@ -700,6 +727,7 @@ int pwm_register(struct pwm_device *p, struct device *parent, int id)
 	return pwm_register_byname(p, parent, name);
 }
 EXPORT_SYMBOL(pwm_register);
+
 
 int pwm_unregister(struct pwm_device *p)
 {
